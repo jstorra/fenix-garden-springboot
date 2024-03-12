55a6b7989ee0149b486f765fc65186504835e69a
jstorra <torragarciaj@gmail.com>
2024-03-12 11:37:55 -0500

    feat: :sparkles: Adding files to start the project

---
+++ fenixgarden/src/main/java/com/example/fenixgarden/FenixgardenApplication.java
@@ -0,0 +1,13 @@
+package com.example.fenixgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenixgardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FenixgardenApplication.class, args);
+	}
+
+}
---
+++ fenixgarden/src/test/java/com/example/fenixgarden/FenixgardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fenixgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenixgardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
