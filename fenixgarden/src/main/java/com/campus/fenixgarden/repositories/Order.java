1ba74e77333d84df66cc9e47ce62ac6dbd3808a3
RCNicolas <114864432+RCNicolas@users.noreply.github.com>
2024-03-12 21:15:43 -0500

    feat: :sparkles: Files repositories - GamaProduct - Product - Order - OrderDetail

---
+++ fenixgarden/src/main/java/com/campus/fenixgarden/repositories/Order.java
@@ -0,0 +1,9 @@
+package com.campus.fenixgarden.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Order extends JpaRepository<Order,Long > {
+
+}
