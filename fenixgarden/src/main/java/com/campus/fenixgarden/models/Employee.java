c61c30e02dd463b4661c53efdf4b014c60afcffa
jstorra <131458634+jstorra@users.noreply.github.com>
2024-03-12 20:34:34 -0500

    feat: :sparkles: Implementing models - Customer - Employee - Office - Payment

--- fenixgarden/src/main/java/com/campus/fenixgarden/models/Employee.java
+++ fenixgarden/src/main/java/com/campus/fenixgarden/models/Employee.java
@@ -1,5 +1,137 @@
 package com.campus.fenixgarden.models;
 
-public class Employee {
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_empleado")
+    private Long employeeCode;
+
+    @Column(name = "nombre", nullable = false)
+    private String name;
+
+    @Column(name = "apellido1", nullable = false)
+    private String lastName1;
+
+    @Column(name = "apellido2")
+    private String lastName2;
+
+    @Column(name = "extension", nullable = false)
+    private String extension;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "puesto")
+    private String rol;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "codigo_oficina")
+    private Office office;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "codigo_jefe")
+    private Employee boss;
+
+    @OneToMany(mappedBy = "repSales", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Customer> customers;
+
+    public Long getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(Long employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName1() {
+        return lastName1;
+    }
+
+    public void setLastName1(String lastName1) {
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName2() {
+        return lastName2;
+    }
+
+    public void setLastName2(String lastName2) {
+        this.lastName2 = lastName2;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Office getOffice() {
+        return office;
+    }
+
+    public void setOffice(Office office) {
+        this.office = office;
+    }
+
+    public Employee getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Employee boss) {
+        this.boss = boss;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeCode=" + employeeCode +
+                ", name='" + name + '\'' +
+                ", lastName1='" + lastName1 + '\'' +
+                ", lastName2='" + lastName2 + '\'' +
+                ", extension='" + extension + '\'' +
+                ", email='" + email + '\'' +
+                ", rol='" + rol + '\'' +
+                ", office=" + office +
+                ", boss=" + boss +
+                '}';
+    }
 }
--- fenixgarden/src/main/java/com/campus/fenixgarden/models/Payment.java
+++ fenixgarden/src/main/java/com/campus/fenixgarden/models/Payment.java
@@ -1,4 +1,78 @@
 package com.campus.fenixgarden.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pago")
 public class Payment {
+    @Id
+    @Column(name = "id_transaccion", nullable = false)
+    private String transactionId;
+
+    @Column(name = "forma_pago", nullable = false)
+    private String wayToPay;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_pago", nullable = false)
+    private LocalDate paymentDate;
+
+    @Column(name = "total", nullable = false)
+    private double total;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "codigo_cliente")
+    private Customer customer;
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getWayToPay() {
+        return wayToPay;
+    }
+
+    public void setWayToPay(String wayToPay) {
+        this.wayToPay = wayToPay;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "transactionId='" + transactionId + '\'' +
+                ", wayToPay='" + wayToPay + '\'' +
+                ", paymentDate=" + paymentDate +
+                ", total=" + total +
+                ", customer=" + customer +
+                '}';
+    }
 }
